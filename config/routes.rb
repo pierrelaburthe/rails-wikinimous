0dbdb20b3be5e816873bdf13d75bc1b284755a4c
Pierre Laburthe <laburthe35@gmail.com>
2017-08-08 18:32:08 +0200

    ending the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-
-  get 'articles/show'
-
-  get 'articles/update'
-
-  get 'articles/create'
-
-  get 'articles/edit'
-
-  get 'articles/destroy'
-
-  get 'articles/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  root to: 'articles#index'
 end
